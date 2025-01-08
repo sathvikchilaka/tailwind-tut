5f0950991144cfced2cf7ca30a9caba90bc68e2c
sathvik <cs34@iitbbs.ac.in>
2025-01-09 01:04:33 +0530

    HeroSection and Features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from '../assets/logo.png';
+import { Menu, X } from 'lucide-react';
 import { navItems } from '../constants';
 
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleMobileDrawer = () => setMobileDrawerOpen(!mobileDrawerOpen);
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm flex items-center justify-between">
@@ -21,7 +26,27 @@ const Navbar = () => {
                 <a href="#" className='border rounded-md px-3 py-2'>Sign In</a>
                 <a href="#" className='rounded-md bg-gradient-to-r from-orange-500 to-orange-800 px-3 py-2 '>Create an account</a>
             </div>
+            <div className="lg:hidden flex flex-col justify-end">
+                <button onClick={toggleMobileDrawer}>
+                    {mobileDrawerOpen? <X/> : <Menu/> }
+                </button>
+            </div>
         </div>
+        {mobileDrawerOpen && (
+            <div className="fixed right-0 z-20 bg-neutral-900 flex flex-col justify-center items-center lg:hidden w-full p-12">
+                <ul>
+                    {navItems.map((item,key)=>(
+                        <li key={key} className='py-4'>
+                            <a href={item.href}> {item.label} </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex space-x-12">
+                <a href="#" className='border rounded-md px-3 py-2'>Sign In</a>
+                <a href="#" className='rounded-md bg-gradient-to-r from-orange-500 to-orange-800 px-3 py-2 '>Create an account</a>
+                </div>
+            </div>
+        )}
     </nav>
   )
 }
